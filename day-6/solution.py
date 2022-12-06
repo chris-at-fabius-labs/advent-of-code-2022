be740fde85065b18e606e2044f8c4573238029a8
Christopher Bonhage <christopherbonhage@fabiuslabs.com>
2022-12-06 11:28:11 -0500

    Minor refactor of Day 6 solution

--- day-6/solution.py
+++ day-6/solution.py
@@ -2,14 +2,13 @@
 
 def find_start(line, count):
     markers = list()
-    for i, c in enumerate(line):
-        markers.append(c)
-        if len(markers) < count: continue
-        if len(set(markers)) == count: return i + 1
-        markers.pop(0)
+    for index, char in enumerate(line):
+        markers.append(char)
+        if len(set(markers)) == count: return index + 1
+        if len(markers) == count: markers.pop(0)
 
 for line in open('input.txt', 'r').readlines():
-    line = line.rstrip('\n')
-    start_of_packet = find_start(line, 4)
-    start_of_message = find_start(line, 14)
-    print(line, start_of_packet, start_of_message)
+    print("Packet:", line.rstrip("\n"))
+    print("Start of Packet:", find_start(line, 4))
+    print("Start of Message:", find_start(line, 14))
+    print("")
