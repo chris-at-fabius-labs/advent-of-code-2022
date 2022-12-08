4bcc9478c2a2c16999124d4f55411661714f1ffd
Christopher Bonhage <christopherbonhage@fabiuslabs.com>
2022-12-08 13:35:16 -0500

    Day 8 Solution

---
+++ day-8/solution.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+ 
+cells = dict()
+row_count = 0
+col_count = 0
+
+def get_perimeter_count():
+    return row_count * 2 + col_count * 2 - 4
+
+def check_visible_from_edge(origin_row, origin_col):
+    return (
+        check_direction1(origin_row, origin_col, 0, -1) or
+        check_direction1(origin_row, origin_col, -1, 0) or
+        check_direction1(origin_row, origin_col, 0, +1) or
+        check_direction1(origin_row, origin_col, +1, 0)
+    )
+
+def get_scenic_score(origin_row, origin_col):
+    return (
+        check_direction2(origin_row, origin_col, 0, -1) *
+        check_direction2(origin_row, origin_col, -1, 0) *
+        check_direction2(origin_row, origin_col, 0, +1) *
+        check_direction2(origin_row, origin_col, +1, 0)
+    )
+
+def check_direction1(origin_row, origin_col, delta_x, delta_y): 
+    origin_value = cells[(origin_row, origin_col)]
+    cursor_row, cursor_col = origin_row, origin_col
+    while True:
+        cursor_row += delta_x
+        cursor_col += delta_y
+        if (
+            cursor_row < 0 or
+            cursor_row >= row_count or
+            cursor_col < 0 or
+            cursor_col >= col_count
+        ): return True
+        cursor_value = cells[(cursor_row, cursor_col)]
+        if cursor_value >= origin_value: return False
+
+def check_direction2(origin_row, origin_col, delta_x, delta_y): 
+    origin_value = cells[(origin_row, origin_col)]
+    cursor_row, cursor_col = origin_row, origin_col
+    seen_count = 0
+    while True:
+        cursor_row += delta_x
+        cursor_col += delta_y
+        if (
+            cursor_row < 0 or
+            cursor_row >= row_count or
+            cursor_col < 0 or
+            cursor_col >= col_count
+        ): break
+        seen_count += 1
+        cursor_value = cells[(cursor_row, cursor_col)]
+        if cursor_value >= origin_value: break
+    return seen_count
+
+# Process the input file into a data structure
+for row_index, line in enumerate(open('input.txt', 'r').readlines()):
+    row_count = max(row_count, row_index + 1)
+    for col_index, char in enumerate(line.rstrip("\n")):
+        cells[(row_index, col_index)] = int(char)
+        col_count = max(col_count, col_index + 1)
+
+# Iterate through inner cells of the grid
+visible_count = get_perimeter_count()
+scenic_scores = list()
+for row_index in range(1, row_count - 1):
+    for col_index in range(1, col_count - 1):
+        if check_visible_from_edge(row_index, col_index):
+            visible_count += 1
+        score = get_scenic_score(row_index, col_index) 
+        scenic_scores.append(score)
+
+print("Step 1 Answer:", visible_count)
+print("Step 2 Answer:", max(scenic_scores))
